ecd0e92cdcc2ff6919fd9cb08177d5ade5eaa776
VitomsCatena <vitomscatenathe1@gmail.com>
2023-12-10 13:00:05 +0300

    javascript file

--- Javascript/myscript.js
+++ Javascript/myscript.js
@@ -5,3 +5,53 @@ menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     menuBtn.classList.toggle('fa-times');
 });
+
+//Review JS Code
+let review = document.querySelector('#review'),
+    dots = document.querySelectorAll('#review-dots li'),
+    reviewContent = document.querySelectorAll('#review-content .review-box'),
+    leftArrow = document.querySelector('#left-arrow'),
+    rightArrow = document.querySelector('#right-arrow'),
+    reviewSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    reviewTimer;
+
+window.onload = function () {
+    function playSlide(slide) {
+        for (let i = 0; i < dots.length; i++) {
+            reviewContent[i].classList.remove('active');
+            reviewContent[i].classList.remove('inactive');
+            dots[i].classList.remove('active');
+        }
+        if (slide < 0) {
+            slide = currentSlide = reviewContent.length - 1;
+        }
+        if (slide > reviewContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+        if (currentActive != currentSlide) {
+            reviewContent[currentActive].classList.add("inactive");
+        }
+        reviewContent[slide].classList.add("active");
+        dots[slide].classList.add("active");
+        currentActive = currentSlide;
+
+        clearTimeout(reviewTimer);
+        reviewTimer = setTimeout(function () {
+            playSlide(currentSlide += 1);
+        }, reviewSpeed)
+    }
+    leftArrow.addEventListener("click", () => {
+        playSlide(currentSlide -= 1);
+    })
+    rightArrow.addEventListener("click", () => {
+        playSlide(currentSlide += 1);
+    })
+    for (let j = 0; j < dots.length; j++) {
+        dots[j].addEventListener("click", () => {
+            playSlide(currentSlide = dots.indexOf(this));
+        })
+    }
+    playSlide(currentSlide);
+}
